0fdde84ce5410419902e306a56d41330b30d3042
Franz Cortez <franz.cortez@alumnos.ucn.cl>
2022-05-31 13:53:15 -0400

    feat(navbar): add a navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import SideBarData from "./SideBarData";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+
+import '../css/Navbar.css';
+import { IconContext } from "react-icons/lib";
+
+function Navbar(props) {
+
+    const value = props.value;
+
+    const [ sidebar,  setSidebar ] = useState(false);
+
+    const showSideBar = () => setSidebar(!sidebar);
+
+    return (
+        <>
+            {
+                value && 
+                <IconContext.Provider value={{color:'#fff'}}>
+                    <div className='navbar'>
+                        <Link to="#" className="menu-bars">
+                            <FaIcons.FaBars onClick={showSideBar}/>
+                        </Link>
+                    </div>   
+
+                    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+                        <ul className="nav-menu-items" onClick={showSideBar}>
+                            <li className="navbar-toggle">
+                                <Link to='#' className="menu-bars">
+                                    <AiIcons.AiOutlineClose/>
+                                </Link>
+                            </li>
+                            {SideBarData.map((item, index) => {
+                                return (
+                                    <li key={index} className={item.cName} >
+                                        <Link to={item.path}>
+                                            {item.icon}
+                                            <span>{item.title}</span>
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                    </nav>
+                </IconContext.Provider>
+            }
+
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/components/SideBarData.jsx
@@ -0,0 +1,26 @@
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+const SideBarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    }
+]
+
+export default SideBarData;
