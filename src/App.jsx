deb4131ad8d2dde4023cb73219f59ad81fb34aaa
Franz Cortez <franz.cortez@alumnos.ucn.cl>
2022-06-01 11:59:12 -0400

    feat: add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,28 @@ import { AuthProvider } from './context/AuthProvider';
 import RutaProtegida from './layout/RutaProtegida';
 import WebLayout from './layout/WebLayout';
 
+import Navbar from './components/Navbar';
 import Login from './paginas/Login';
+import Home from './paginas/Home';
 
 function App() {
 
   return (
+
     <BrowserRouter>
       <AuthProvider>
+        <Navbar value={false} />
         <Routes>
+          
           <Route path='/' element={<WebLayout />}>
 
           </Route>
 
-
-
           <Route path='/admin' element={<RutaProtegida />}>
             <Route index element={<Login />}/>
+            <Route path='home' element={<Home />}/>
           </Route>
+
         </Routes>
       </AuthProvider>
     </BrowserRouter>
